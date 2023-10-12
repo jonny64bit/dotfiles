774471af49d315807dc2c882fcdba757dbc83098
Jonathan Dent <jon@jdent.co.uk>
2023-10-12 16:40:47 +0100

    fix: 🐛 copilot now loads correcting with cmp is loaded

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,8 +1,14 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-        require("copilot").setup({})
-    end,
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+  },
+  filetypes = {
+    cs = true,
+    csharp = true,
+    ts = true,
+  }
 }
